83af93c9bc4596b0d9d4b1a90e5bbc3f7daf0448
HirbodBehnam <chrome.hiri.angry@gmail.com>
2022-03-09 16:37:25 +0330

    Added progress bar Added GitHub Actions for release

--- go.mod
+++ go.mod
@@ -8,13 +8,20 @@ require (
 )
 
 require (
+	github.com/VividCortex/ewma v1.1.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.2 // indirect
+	github.com/cheggaaa/pb/v3 v3.0.8 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/go-faster/errors v0.5.0 // indirect
 	github.com/go-faster/jx v0.31.0 // indirect
 	github.com/go-faster/xor v0.3.0 // indirect
 	github.com/gotd/ige v0.2.2 // indirect
 	github.com/gotd/neo v0.1.5 // indirect
 	github.com/klauspost/compress v1.14.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/segmentio/asm v1.1.3 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
--- uploader.go
+++ uploader.go
@@ -5,10 +5,12 @@ import (
 	"TelegramFileUploader/types"
 	"context"
 	"fmt"
+	"github.com/cheggaaa/pb/v3"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/message"
 	"github.com/gotd/td/telegram/uploader"
 	"github.com/gotd/td/tg"
+	"os"
 )
 
 // BeginBot starts the bot and tries to upload the file to Telegram
@@ -17,7 +19,7 @@ func BeginBot(ctx context.Context, client *telegram.Client) error {
 	u := uploader.NewUploader(api)
 	sender := message.NewSender(api).WithUploader(u)
 	// Upload the file
-	upload, err := u.FromPath(ctx, filePath)
+	upload, err := uploadWithProgress(ctx, u)
 	if err != nil {
 		return fmt.Errorf("upload %q: %w", filePath, err)
 	}
@@ -52,4 +54,27 @@ func BeginBot(ctx context.Context, client *telegram.Client) error {
 	_, err = sender.Resolve(receiverID).Media(ctx, media)
 	// Done
 	return err
+}
+
+func uploadWithProgress(ctx context.Context, u *uploader.Uploader) (tg.InputFileClass, error) {
+	// Open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+	// Get file size
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	// Set up the progress bar
+	bar := pb.Full.Start64(fileInfo.Size())
+	bar.Set(pb.SIBytesPrefix, true)
+	barReader := bar.NewProxyReader(file)
+	defer bar.Finish()
+	// Upload the file
+	return u.Upload(ctx, uploader.NewUpload(fileInfo.Name(), barReader, fileInfo.Size()))
 }
